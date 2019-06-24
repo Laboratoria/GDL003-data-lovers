513534d2dd2a237d3d7f14740687ea8750ff4e51
Kaviza90 <vieralaqueviera@gmail.com>
2019-06-24 15:49:27 -0500

    se agrega un parrafo a html

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,21 @@
-require('../src/data.js');
+require("../src/data.js");
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe("example", () => {
+  it("is a function", () => {
+    expect(typeof example.example).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it("returns `example`", () => {
+    expect(example.example()).toBe("example");
+  });
+});
+
+describe("filtrado", () => {
+  it("is a function", () => {
+    expect(typeof example.filtrado).toBe("function");
+  });
+
+  it("returns `filtrado`", () => {
+    expect(example.filtrado()).toBe("filtrado");
   });
 });
