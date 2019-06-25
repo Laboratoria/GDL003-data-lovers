057279fa57b6671d8f7c9f60cd4f89ddf1bd3227
carmenlab <cavagml@gmail.com>
2019-06-25 15:06:47 -0500

    Se subieron fotos

--- src/data.js
+++ src/data.js
@@ -1,8 +1,38 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
+  const filterData= (data, condition) => {
+    return 'filterData';
+  };
+
+  const sortData = (data, sortOrder) => {
+    return 'sortData';
+  };
+
+  const computeStats = (data) => {
+    return 'computeStats';
+  };
+
+  window.dataLovers = {
+    filterData,
+    sortData,
+    computeStats
+};
+
+
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
+/*
 const example = () => {
   return 'example';
 };
 
-window.example = example;
+const filtrado = () => {
+  return 'filtrado';
+};
+
+window.example = {
+  example,
+  filtrado
+};
+*/
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,64 @@
 require('../src/data.js');
+/*
+describe('dataLovers', () => {
+
+  it('debería ser un objeto', () => {
+    expect(typeof dataLovers).toBe('object');
+  });*/
+
+  describe('filterData', () => {
+    it('is a function', () => {
+      expect(typeof dataLovers.filterData).toBe('function');
+    });
+
+    it('returns `filterData`', () => {
+      expect(dataLovers.filterData()).toBe('filterData');
+    });
+  });
+
+  describe ('sortData', () => {
+    it ('is a function', () => {
+      expect (typeof dataLovers.sortData).toBe('function');
+    });
+
+    it('returns `sortData`', () => {
+      expect(dataLovers.sortData()).toBe('sortData');
+    });
+  });
+
+  describe ('computeStats', () => {
+    it ('is a function', () => {
+      expect (typeof dataLovers.computeStats).toBe('function');
+    });
+
+    it('returns `computeStats`', () => {
+      expect(dataLovers.computeStats()).toBe('computeStats');
+    });
+  });
+/*
+});
+
+
+require('../src/data.js');
 
 
 describe('example', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof example.example).toBe('function');
   });
 
   it('returns `example`', () => {
-    expect(example()).toBe('example');
+    expect(example.example()).toBe('example');
   });
 });
+
+describe ('filtrado', () => {
+  it ('is a function', () => {
+    expect (typeof example.filtrado).toBe('function');
+  });
+
+  it('returns `filtrado`', () => {
+  expect(example.filtrado()).toBe('filtrado');
+  });
+});
+*/
