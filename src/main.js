d56f53532269b1b919cd0c47762cae853813e493
carmenlab <cavagml@gmail.com>
2019-06-26 18:57:06 -0500

    Mostrar Datos sin document write

--- src/main.js
+++ src/main.js
@@ -2,16 +2,25 @@
 
 let uno = Object.keys(window.LOL.data);
 
-  for(let i=0; i<=uno.length; i++){
-    let vista  = uno[i];
-    let muestra=JSON.stringify(vista);
-  document.write(vista+'<br>');
+  for (let x in uno) {
+    document.getElementById("root").innerHTML += uno[x] + "<br>";
+console.log(x);
 
+  };
+/* for(let i=0; i<=uno.length; i++){
+   let vista  = uno[i];
+   let muestra=JSON.stringify(vista);
+  document.write(vista+'<br>');
+*/
   //console.log(uno[i]);
   //console.log(vista);
-  console.log(muestra);
-};
+ // console.log(muestra);
+//};
 
+//dataLol =window.LOL.data;
+//let x = dataLol;
+//document.getElementById("root").innerHTML = x;
+//console.log(x);
   //let datos=window.LOL.data;
   //let muestra=JSON.stringify(datos); //volver los datos de un arreglo en string
   //let nombre=JSON.parse(muestra);
