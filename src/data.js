27fb729bc01bf88d105760eeba74d6fe3fb27231
MariaZanahoria <m.iselacarrillo.p@gmail.com>
2019-07-02 18:57:58 -0500

    Mostrar todos los pokemones

--- src/data.js
+++ src/data.js
@@ -1,32 +1,6 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-//const example = () => {
-//  return "example";
-//};
-
-//window.example = example
-
 window.dataLovers = {
+//Declaración para mostrar todos los pokémon
+//const : showAll = POKEMON.pokemon.map(allPokemon => allPokemon)
+};
+//console.log(showAll);
 
-   filterByName: (data, name) => {
-    let namePokemon ="";
-      if (name.value == "name"){
-        let filterName = data.filter(data => data>name);
-        console.log(filterName);
-      }
-    }   
-        //for (let pokemon = 0;pokemon<data.lenght;pokemon++){ 
-        //let filterName=data[pokemon].name;
-        //namePokemon+=pokemon+'<br>';
-  
-/*  getPokemonByNum: number => {
-    return pokemonData.array.find(p => p.number === number);
-  },
-  getPokemonByTypeFilter: type => {
-    return pokemonData.array.filter(p => p.type.indexOf(type) > -1);
-  },
-  getPokemonByWeaknessesFilter: weaknesses => {
-    return pokemonData.array.filter(p => p.weaknesses.indexOf(weaknesses) > -1);
-  }
-};*/}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
-let buttonSearch = document.getElementById("search")
-let name = document.querySelector(".name");
-  buttonSearch.addEventListener("click", ()=>{
-      let dataPokemon = window.dataLovers.filterByName(POKEMON.pokemon, name);
-      document.getElementById("result").innerHTML = dataPokemon
+let buttonAllPokemon = document.getElementById('showAllPokemon');
+const container = document.getElementById('contentAll');
+  buttonAllPokemon.addEventListener("click", () => {
+    container.innerHTML="";
+  let all = buttonAllPokemon.value;
+  let  allPokemonInformation = POKEMON.pokemon.forEach(allInformation => {
+    console.log(allInformation);
+    container.innerHTML += allInformation.num + allInformation.name + allInformation.image +allInformation.type;
   });
+});
+
+// tipos de pokemon
+let typeGrass = document.getElementById('Grass');
+let typePoison = document.getElementById('Poison');
+let typeFire = document.getElementById('Fire');
+let typeFlying = document.getElementById('Flying');
+let typeWater= document.getElementById('Water');
+let typeBug = document.getElementById('Bug');
+let typeNormal = document.getElementById('Normal');
+let typeElectric = document.getElementById('Electric');
+let typeGround = document.getElementById('Ground');
+let typeFighting = document.getElementById('Fighting');
+let typePsychic = document.getElementById('Psychic');
+let typeRock = document.getElementById('Rock');
+let typeIce = document.getElementById('Ice');
+let typeGhost = document.getElementById('Ghost');
+let typeDragon = document.getElementById('Dragon');
+
+//let buttonSearch = document.getElementById("search")
+ // buttonSearch.addEventListener("click", ()=>{
+   //   let dataPokemon = window.dataLovers.filterPokemon();
+     // document.getElementById("result").innerHTML = dataPokemon
+ // });
